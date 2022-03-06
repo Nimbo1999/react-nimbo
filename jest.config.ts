44fe353c6d8f49eb360f0002a594c4d3681b2a3b
Matheus Lopes <matlopes1999@gmail.com>
2022-03-06 12:13:08 -0300

    Add the first test on button component and finish the jest & testing library configuration

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,11 @@
 import type { Config } from '@jest/types';
 
 const jestConfig: Config.InitialOptions = {
-    verbose: true
+    verbose: true,
+    rootDir: './src',
+    moduleDirectories: ['node_modules'],
+    testEnvironment: 'jest-environment-jsdom',
+    setupFilesAfterEnv: ['<rootDir>/setupTests.ts']
 };
 
 export default jestConfig;
---
+++ src/Button/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+import type { ButtonProps } from './Button.types';
+
+const Button: FC<ButtonProps> = ({ children, ...props }) => {
+    return <button {...props}>{children}</button>;
+};
+
+export default Button;
