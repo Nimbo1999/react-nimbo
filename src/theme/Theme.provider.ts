bf45efce00a2d21a9b050ee3cbb5bf3b38462dd0
Matheus Lopes <matlopes1999@gmail.com>
2022-03-28 21:56:30 -0300

    Add the color theme version 1

--- src/theme/Theme.provider.ts
+++ src/theme/Theme.provider.ts
@@ -4,9 +4,27 @@ const BASE_UNIT = Number(process.env.STYLE_BASE_UNIT) || 8;
 
 const theme: DefaultTheme = {
     palette: {
-        white: '#ffffff',
-        black: '#000000'
+        white: '#E2E2E2',
+        background: {
+            base: '#02010D',
+            light: '#F5F4FB'
+        },
+        primary: {
+            base: '#6D4CF1',
+            light: '#B6A7F1'
+        },
+        util: {
+            success: '#00AB11',
+            error: '#B90F0F',
+            warning: '#E3B71B',
+            info: '#00A1AB'
+        },
+        grey: {
+            base: '#74727A'
+        }
     },
+
+    borderRadius: '6px',
 
     spacing: (value: number) => `${BASE_UNIT * value}px`
 };
--- src/theme/theme.d.ts
+++ src/theme/theme.d.ts
@@ -4,8 +4,26 @@ declare module 'styled-components' {
     export interface DefaultTheme {
         palette: {
             white: string;
-            black: string;
+            background: {
+                base: string;
+                light: string;
+            };
+            primary: {
+                base: string;
+                light: string;
+            };
+            util: {
+                success: string;
+                error: string;
+                warning: string;
+                info: string;
+            };
+            grey: {
+                base: string;
+            };
         };
+
+        borderRadius: string;
 
         spacing: (value: number) => string;
     }
